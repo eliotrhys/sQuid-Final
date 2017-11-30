75c331ccd3bf3c9145e028e2a3785187241a50e5
eliotrhys <eliot@eliotshort.co.uk>
2017-11-30 09:27:28 +0000

    thats a safety bounce

--- controllers/merchants_controller.rb
+++ controllers/merchants_controller.rb
@@ -15,3 +15,13 @@ get '/merchants/by_merchant/:id' do #TRANSACTIONS BY MERCHANT
   @transaction_result = Transaction.by_tag(params[:id])
   erb(:"merchants/by_merchant_id")
 end
+
+get '/new_merchant' do #NEW
+  erb(:"merchants/new")
+end
+
+post '/merchants/by_merchant' do #CREATE
+  merchant = Merchant.new(params)
+  merchant.save
+  redirect to ("/merchants/by_merchant")
+end
